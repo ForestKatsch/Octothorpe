e42b395168756aaaa2f971328ffc030c4abc54cb
Forest Katsch <me@forestkatsch.com>
2024-02-01 19:58:20 -0800

    Initial Commit

---
+++ Octothorpe/OctothorpeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OctothorpeApp.swift
+//  Octothorpe
+//
+//  Created by Forest Katsch on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct OctothorpeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
